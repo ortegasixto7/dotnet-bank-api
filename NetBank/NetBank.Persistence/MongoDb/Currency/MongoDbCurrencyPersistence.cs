134607525492141c5fad51ed26a0574e231e12d8
Sixto Ortega <ortegasixto7@gmail.com>
2022-09-16 15:39:48 -0400

    Adding currency persistence mongodb implementation

---
+++ NetBank/NetBank.Persistence/MongoDb/Currency/MongoDbCurrencyPersistence.cs
@@ -0,0 +1,47 @@
+﻿using NetBank.Core.Currency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBank.Persistence.MongoDb.Currency
+{
+    public class MongoDbCurrencyPersistence : ICurrencyPersistence
+    {
+        public void Create(Core.Currency.Currency data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Core.Currency.Currency GetActiveByCodeOrException(string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Core.Currency.Currency> GetAllActive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Core.Currency.Currency> GetAllInactive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Core.Currency.Currency GetByCodeOrException(string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Core.Currency.Currency? GetByCodeOrNull(string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Core.Currency.Currency data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
