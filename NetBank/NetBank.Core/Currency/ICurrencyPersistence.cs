c0df901edc11f14c997ef04f58dfc80a8000485e
Sixto Ortega <ortegasixto7@gmail.com>
2022-09-16 15:27:28 -0400

    Adding currency persistence and model

---
+++ NetBank/NetBank.Core/Currency/ICurrencyPersistence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBank.Core.Currency
+{
+    public interface ICurrencyPersistence
+    {
+        void Create(Currency data);
+        void Update(Currency data);
+        Currency? GetByCodeOrNull(string code);
+        Currency GetByCodeOrException(string code);
+        Currency GetActiveByCodeOrException(string code);
+        IEnumerable<Currency> GetAllActive();
+        IEnumerable<Currency> GetAllInactive();
+    }
+}
