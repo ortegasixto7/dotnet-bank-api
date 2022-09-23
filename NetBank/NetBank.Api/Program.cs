cf71a5f8064a18d7d55b42591e533735d223a9df
Sixto Ortega <ortegasixto7@gmail.com>
2022-09-23 15:19:51 -0400

    Refactoring program cs file

--- NetBank/NetBank.Api/Program.cs
+++ NetBank/NetBank.Api/Program.cs
@@ -1,3 +1,6 @@
+using NetBank.Core.Currency;
+using NetBank.Persistence.MongoDb.Currency;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<ICurrencyPersistence, MongoDbCurrencyPersistence>();
 
 var app = builder.Build();
+DotNetEnv.Env.Load();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
