e2a320d6e8a11c6597fb6aa6c55b5c013e7f235c
Sixto Ortega <ortegasixto7@gmail.com>
2022-09-23 15:18:34 -0400

    Adding customs exceptions

---
+++ NetBank/NetBank.Core/Validation/CustomException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBank.Core.Validation
+{
+    public abstract class CustomException
+    {
+        public static readonly string CurrencyNotFound = "CURRENCY_NOT_FOUND";
+    }
+}
