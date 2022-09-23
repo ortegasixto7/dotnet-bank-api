3bf5e734d2d42c571eb01a684e6251f2945e312c
Sixto Ortega <ortegasixto7@gmail.com>
2022-09-23 16:26:09 -0400

    Adding use case interface

---
+++ NetBank/NetBank.Core/IUseCase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBank.Core
+{
+    public interface IUseCaseCommand<T>
+    {
+        Task Execute(T request);
+    }
+
+    public interface IUseCaseQuery<T>
+    {
+        Task<dynamic> Execute(T request);
+    }
+}
