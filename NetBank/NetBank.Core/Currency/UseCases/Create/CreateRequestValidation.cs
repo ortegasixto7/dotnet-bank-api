1649d7b56535db3053afa1b311b24c18c88e88b7
Sixto Ortega <ortegasixto7@gmail.com>
2022-09-23 16:25:51 -0400

    Adding create currency use case

---
+++ NetBank/NetBank.Core/Currency/UseCases/Create/CreateRequestValidation.cs
@@ -0,0 +1,18 @@
+﻿using NetBank.Core.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBank.Core.Currency.UseCases.Create
+{
+    public class CreateRequestValidation : IRequestValidator<CreateRequest>
+    {
+        public void Validate(CreateRequest request)
+        {
+            if (string.IsNullOrEmpty(request.Code)) throw new BadRequestException(CustomExceptionCodes.CodeIsRequired);
+            if (string.IsNullOrEmpty(request.Symbol)) throw new BadRequestException(CustomExceptionCodes.SymbolIsRequired);
+        }
+    }
+}
---
+++ NetBank/NetBank.Core/Currency/UseCases/Create/CreateUseCase.cs
@@ -0,0 +1,32 @@
+﻿using NetBank.Core.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetBank.Core.Currency.UseCases.Create
+{
+    public class CreateUseCase : IUseCaseCommand<CreateRequest>
+    {
+        private readonly ICurrencyPersistence currencyPersistence;
+
+        public CreateUseCase(ICurrencyPersistence currencyPersistence)
+        {
+            this.currencyPersistence = currencyPersistence;
+        }
+
+        public async Task Execute(CreateRequest request)
+        {
+            new CreateRequestValidation().Validate(request);
+            var currency = new Currency
+            {
+                Code = request.Code,
+                Symbol = request.Symbol
+            };
+            var existDuplicatedCode = await currencyPersistence.GetByCodeOrNullAsync(request.Code);
+            if (existDuplicatedCode != null) throw new BadRequestException(CustomExceptionCodes.CurrencyCodeAlreadyExist);
+            await currencyPersistence.CreateAsync(currency);
+        }
+    }
+}
